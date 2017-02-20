391655e88163a42cd2ee132474322ff914a0c213
Dempsey <edempsey@skylinetechnologies.com>
2017-02-20 16:19:33 -0600

    Updated utterances and responses

--- src/alexa/intents/about.js
+++ src/alexa/intents/about.js
@@ -4,24 +4,30 @@ var intent = {
         "FIRST_NAME": "AMAZON.US_FIRST_NAME"
     },
     utterances: [
-        "{-|FIRST_NAME}"
+        "{about |}{-|FIRST_NAME}"
     ]
 };
 intent.handler = function(request, response) {
     //response.say("Well, okay then bye").shouldEndSession(true).send();
 
-    var name = response.slot("FIRST_NAME");
+    var name = request.slot("FIRST_NAME");
+    console.log(name);
     var msg = "";
 
-    if(name === "malachi"){
+    if(name === "Malachi"){
         msg = `<p>Hey, who are you again?</p><p>Got that right</p>`;
     }
-    else if(name === "hannah" || name ==="hanah" || name === "hana"){
+    else if(name === "Hannah" || name ==="Hanah" || name === "Hana"){
         msg = `<p>I have something I need to tell you.</p><p>oh man you are so fast, I love you too</p>`;
     }
-    else if(name === "levi"){
-        msg = '<p>Who am I?</p><p>Got that right.</p>';
+    else if(name === "Levi"){
+        msg = '<p>Who am I?</p><p>Got that right</p>';
     }
+    else{
+        msg = `I am sorry, I didn't quite understand what you said.  Please try again.`;
+    }
+
+    console.log(msg);
 
     response.say(msg).shouldEndSession(true).send();
 }
