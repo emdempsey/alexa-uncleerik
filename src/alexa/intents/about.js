da72762ea55ca33c57c8f5f000b8c5ef2c7d3f18
Dempsey <edempsey@skylinetechnologies.com>
2017-02-20 20:07:56 -0600

    Changed delay to 1000ms

--- src/alexa/intents/about.js
+++ src/alexa/intents/about.js
@@ -13,15 +13,16 @@ intent.handler = function(request, response) {
     var name = request.slot("FIRST_NAME");
     console.log(name);
     var msg = "";
+    var delay = "1000ms";
 
     if(name === "Malachi"){
-        msg = `Hey, who are you again?<break time="750ms"/>Got that right`;
+        msg = `Hey, who are you again?<break time="${delay}"/>Got that right`;
     }
     else if(name === "Hannah" || name ==="Hanah" || name === "Hana"){
-        msg = `I have something I need to tell you.<break time="750ms"/>oh man you are so fast, I love you too`;
+        msg = `I have something I need to tell you.<break time="${delay}"/>oh man you are so fast, I love you too`;
     }
     else if(name === "Levi"){
-        msg = 'Who am I?<break time="750ms"/>Got that right';
+        msg = `Who am I?<break time="${delay}"/>Got that right`;
     }
     else{
         msg = `I am sorry, I didn't quite understand what you said.  Please try again.`;
