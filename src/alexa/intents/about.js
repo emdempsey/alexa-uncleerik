5f88b6a0a59452739917f0dfee099ba95f2a5419
Dempsey <edempsey@skylinetechnologies.com>
2017-02-23 10:39:30 -0600

    initial audio file feature

--- src/alexa/intents/about.js
+++ src/alexa/intents/about.js
@@ -14,9 +14,12 @@ intent.handler = function(request, response) {
     console.log(name);
     var msg = "";
     var delay = "1000ms";
+    var endSession = true;
+    var audioUrl = "https://4216fbdd.ngrok.io/audio/";
 
     if(name === "Malachi"){
-        msg = `Hey, who are you again?<break time="${delay}"/>Got that right`;
+        audioUrl += "kids.mp3";
+        msg = `<audio src=${audioUrl} />`;//`Hey, who are you again?<break time="${delay}"/>Got that right`;
     }
     else if(name === "Hannah" || name ==="Hanah" || name === "Hana"){
         msg = `I have something I need to tell you.<break time="${delay}"/>oh man you are so fast, I love you too`;
@@ -25,12 +28,13 @@ intent.handler = function(request, response) {
         msg = `Who am I?<break time="${delay}"/>Got that right`;
     }
     else{
-        msg = `I am sorry, I didn't quite understand what you said.  Please try again.`;
+        msg = `<p>I am sorry, I didn't quite understand what you said.</p><p>Please say the name again.</p>`;
+        endSession = false;
     }
 
     console.log(msg);
 
-    response.say(msg).shouldEndSession(true).send();
+    response.say(msg).shouldEndSession(endSession).send();
 }
 
 module.exports = intent;
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 var bodyParser = require("body-parser");
 var express = require("express");
+var path = require("path");
 
 var env = process.env.NODE_ENV || 'dev';
 var config = { port: process.env.PORT || 3000 };
@@ -11,6 +12,9 @@ app.use(bodyParser.json());
 require('./routes-prod').configure(app);
 
 app.use("/alexa/tools", express.static("node_modules/utterance-builder-gui/www"));
+
+var audioFolder = path.resolve(__dirname + "/../audio");
+app.use("/audio",express.static(audioFolder));
 
 // Start the web server on the specified port
 app.listen(config.port, process.env.IP, function() {
